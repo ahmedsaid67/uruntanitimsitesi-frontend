b3e5170ec303b16dc6447392f8707b8a8c474122
ahmedsaid67 <ahmetsaiddemirtas@gmail.com>
2024-06-30 15:01:19 +0300

    Güncelleme açıklaması

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['127.0.0.1'], // Protokol ve sondaki eğik çizgi olmadan
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,40 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import Layout from '../compenent/Layout';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { wrapper } from '../context/provider';
+
+import React, { useRef, useEffect } from "react";
+import { useRouter } from "next/router";
+
+
+
+
+const ROUTES_TO_RETAIN = ["/"];
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const retainedReduxState = useRef(null);
+
+  const isRetainableRoute = ROUTES_TO_RETAIN.includes(
+    router.asPath === "/" ? "/index" : router.asPath
+  );
+
+  useEffect(() => {
+    if (isRetainableRoute) {
+      retainedReduxState.current = pageProps; 
+    }
+  }, [pageProps, isRetainableRoute]);
+
+  return (
+    <Layout>
+      {isRetainableRoute && retainedReduxState.current && (
+        <Component {...retainedReduxState.current} />
+      )}
+      {!isRetainableRoute && <Component {...pageProps} />}
+    </Layout>
+  );
 }
+
+
+
+export default wrapper.withRedux(MyApp);
