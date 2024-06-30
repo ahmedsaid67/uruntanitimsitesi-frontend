d9b2a9286f037c64bec74437e759cfd9bb276d6f
fatihkeless <fatihkeles98@gmail.com>
2024-06-30 16:39:43 +0300

    folder in folder delete
    klasörler içinde olan karışıklık düzeltildi

--- my-next-project/next.config.js
+++
@@ -1,9 +0,0 @@
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['127.0.0.1'], // Protokol ve sondaki eğik çizgi olmadan
-  },
-}
-
-module.exports = nextConfig;
