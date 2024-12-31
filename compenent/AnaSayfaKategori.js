c18a4b12207629761d6d512ef6227214cffdb775
Fatih Keleş <41598238+fatihkeless@users.noreply.github.com>
2025-01-01 01:42:02 +0300

    front update 9.4
    küçük güncellemeler

--- compenent/AnaSayfaKategori.js
+++ compenent/AnaSayfaKategori.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import styles from '../styles/AnaSayfaKategori.module.css';
 import Link from 'next/link';
-import { style } from '@mui/system';
 import { API_ROUTES } from '@/utils/constants';
 
 const AnaSayfaKategori = () => {
@@ -18,44 +17,38 @@ const AnaSayfaKategori = () => {
       });
   }, []);
 
-
-
   return (
-    <div>
+    <div className={styles.container}>
+      <h2 className={styles.kategoriBaslik}>Kategoriler</h2>
       {kategoriler.length === 0 ? (
         <div className={styles.placeholder}></div>
       ) : (
-          <div className={styles.kategoriContainer}>
-            <div className={styles.kategoriBaslik}>Kategoriler</div>
-            <div className={styles.kategoriListesi}>
-              {kategoriler.map((kategori, index) => (
-                <Link 
-                  href={`/urunlerimiz?tab=${kategori.slug}`}
-                  key={kategori.id}
-                  className={`${styles.kategoriItem} ${
-                    index === 0 && kategoriler.length % 2 !== 0 ? styles.kategoriItemFull : ''
-                  }`}
-                >
-                  <img
-                    src={kategori.kapak_fotografi}
-                    alt={kategori.baslik}
-                    className={styles.kategoriGorsel}
-                  />
-                </Link>
-              ))}
-            </div>
-          </div>
+        <div className={styles.kategoriListesi}>
+          {kategoriler.map((kategori, index) => (
+            <Link
+              href={`/urunlerimiz?tab=${kategori.slug}`}
+              key={kategori.id}
+              className={`
+                ${styles.kategoriItem}
+                ${
+                  index === 0 && kategoriler.length % 2 !== 0
+                    ? styles.kategoriItemFull
+                    : styles.kategoriItemHalf
+                }
+              `}
+            >
+              <img
+                src={kategori.kapak_fotografi}
+                alt={kategori.baslik}
+                className={styles.kategoriResim}
+              />
+             
+            </Link>
+          ))}
+        </div>
       )}
     </div>
   );
 };
 
 export default AnaSayfaKategori;
-
-
-
-
-
-
-
-
